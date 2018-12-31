b4aa48c040e995f092369988988afc7fdbc13948
Hendra juice <hd.brandoz@gmail.com>
2018-12-31 19:58:42 +0700

    initial commit

---
+++ app/router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.send("Homepage"));
+router.get('/about', (req, res) => res.send("About page"));
+
+export default router;
---
+++ main.js
@@ -0,0 +1,4 @@
+import app from './app/app';
+
+const port = 9999;
+app.listen(port, () => console.log(`App run on port ${port}`));
