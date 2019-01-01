7682aeb17344f9d9bf32b0d4faf9cee6f4861c04
Hendra juice <hd.brandoz@gmail.com>
2019-01-01 14:23:50 +0700

    enhanced user controller

---
+++ app/controllers/Controller.js
@@ -0,0 +1,6 @@
+export default class Controller {
+  static create(req, res) {
+    // using this for check request
+    console.log("Parent class called!");
+  }
+}
--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,7 +1,16 @@
-const UserController = {
-  index: (req, res) => res.send("User Index"),
-  create: (req, res) => res.send("User Create"),
-  update: (req, res) => res.send("User Update"),
-}
+import Controller from './Controller';
 
-export default UserController;
+export default class UserController extends Controller {
+  static index(req, res) {
+    res.send("User Index");
+  }
+
+  static create(req, res) {
+    super.create(req, res);
+    res.send("User Create");
+  }
+
+  static update(req, res) {
+    res.send("User Update")
+  }
+}
