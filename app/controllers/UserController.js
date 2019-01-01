52867546267a38c6effb182cb072b12b447b831a
Hendra juice <hd.brandoz@gmail.com>
2019-01-01 14:08:26 +0700

    create user controller

---
+++ app/controllers/UserController.js
@@ -0,0 +1,7 @@
+const UserController = {
+  index: (req, res) => res.send("User Index"),
+  create: (req, res) => res.send("User Create"),
+  update: (req, res) => res.send("User Update"),
+}
+
+export default UserController;
--- app/router.js
+++ app/router.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import UserController from "./controllers/UserController";
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send("Homepage"));
 router.get('/about', (req, res) => res.send("About page"));
+
+router.get('/user/create', (req, res) => UserController.create(req, res));
 
 export default router;
