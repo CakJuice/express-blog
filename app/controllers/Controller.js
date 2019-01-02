ffc9f7362f29ba360079a99b1af57e565ededc0b
Hendra Juice <hd.brandoz@gmail.com>
2019-01-02 16:08:06 +0700

    test user controller create user

--- app/controllers/Controller.js
+++ app/controllers/Controller.js
@@ -1,8 +1,30 @@
+import { createRenderer } from 'vue-server-renderer';
+import { readFileSync } from 'fs';
 import path from 'path';
 
 export default class Controller {
+  constructor(layoutPath) {
+    this.layoutPath = layoutPath || path.join(__dirname, '..', 'views/layout/main_layout.html');
+  }
+
   static create(req, res) {
     // using this for check request
     console.log("Parent class called!");
+  }
+
+  renderTemplate(app, context, res) {
+    const renderer = createRenderer({
+      template: readFileSync(this.layoutPath, 'utf-8'),
+    });
+
+    renderer.renderToString(app, context, (err, html) => {
+      if (err) {
+        res.status(500).end('Internal Server Error');
+        console.log(err);
+        return;
+      }
+
+      res.end(html);
+    });
   }
 }
--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,38 +1,42 @@
 import Controller from './Controller';
 import Vue from 'vue';
-import { createRenderer } from 'vue-server-renderer';
 import { readFileSync } from 'fs';
 import path from 'path';
 
 export default class UserController extends Controller {
+  constructor(templateName) {
+    super();
+    this.templatePath = path.join(__dirname, '..', 'views/user', templateName);
+    this.context = {
+      title: "User",
+      meta_keywords: 'user',
+      meta_description: 'user',
+    };
+  }
+
   static index(req, res) {
     res.send("User Index");
   }
 
   static create(req, res) {
     super.create(req, res);
-    const template = path.join(__dirname, '..', 'views/user/create.html');
+    const controller = new UserController('create.html');
+
+    controller.context = {
+      title: "Register New User",
+      meta_keywords: "register,user,signup",
+      meta_description: "Register new user for this blog site",
+    };
+
     const app = new Vue({
       data: {
         url: req.url,
+        someValue: 'Blablabla',
       },
-      template: `<div>The visited URL is: {{ url }}</div>`
+      template: readFileSync(controller.templatePath, 'utf-8').toString(),
     });
 
-    const renderer = createRenderer({
-      template: readFileSync(template, 'utf-8'),
-    });
-
-    renderer.renderToString(app, (err, html) => {
-      if (err) {
-        res.status(500).end('Internal Server Error');
-        return;
-      }
-
-      res.end(html);
-    });
-    
-    // res.send("User Create");
+    controller.renderTemplate(app, controller.context, res);
   }
 
   static update(req, res) {
