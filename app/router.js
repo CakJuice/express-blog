e63f3463d19ff17920460f4db42425b379578db3
Hendra Juice <hd.brandoz@gmail.com>
2019-01-03 10:01:22 +0700

    update user controller

--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import userController from "./controllers/UserController";
+import userController from "./controllers/userController";
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send("Homepage"));
 router.get('/about', (req, res) => res.send("About page"));
 
-router.get('/user', (req, res) => UserController.index(req, res));
+router.get('/user', (req, res) => userController.index(req, res));
 
 export default router;
